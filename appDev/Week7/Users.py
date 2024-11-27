916b6fb11b346ad8ef5129c6c340af871fcb8b57
JT11-11 <jaspertsh08@gmail.com>
2024-11-27 14:41:14 +0800

    finish week 7c practical

---
+++ appDev/Week7/Users.py
@@ -0,0 +1,30 @@
+class User:
+    count_id = 0
+    
+    def __init__(self, first_name, last_name, gender, membership, remarks):
+        User.count_id += 1
+        self.__user_id = User.count_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__gender = gender
+        self.__membership = membership
+        self.__remarks = remarks
+
+    def get_user_id(self):
+        return self.__user_id
+
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_membership(self):
+        return self.__membership
+
+    def get_remarks(self):
+        return self.__remarks
+
